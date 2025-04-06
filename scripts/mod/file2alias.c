1282bca01093049d027fe6588d55c664c4baf5c6
Yassine Oudjana <y.oudjana@protonmail.com>
2025-04-06 14:07:43 +0000

    net: qrtr: Turn QRTR into a bus
    Implement a QRTR bus to allow for creating drivers for individual QRTR
    services. With this in place, devices are dynamically registered for QRTR
    services as they become available, and drivers for these devices are
    matched using service and instance IDs.
    
    In smd.c, replace all current occurences of qdev with qsdev in order to
    distinguish between the newly added QRTR device which represents a QRTR
    service with the existing QRTR SMD device which represents the endpoint
    through which services are provided.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>
    [msm8974: adapted do_qrtr_entry to old-style file2alias API]
    Signed-off-by: Marc Lainez <marc.lainez@gmail.com>

--- scripts/mod/file2alias.c
+++ scripts/mod/file2alias.c
@@ -1503,6 +1503,20 @@ static int do_coreboot_entry(const char *filename, void *symval, char *alias)
 	return 1;
 }
 
+/*
+ * Looks like: qrtr:N:N
+ * Based on Yassine Oudjana's new-style implementation, adapted to old-style
+ * file2alias API by Marc Lainez <marc.lainez@gmail.com>
+ */
+static int do_qrtr_entry(const char *filename, void *symval, char *alias)
+{
+	DEF_FIELD(symval, qrtr_device_id, service);
+	DEF_FIELD(symval, qrtr_device_id, instance);
+	sprintf(alias, "qrtr:%x:%x", service, instance);
+
+	return 1;
+}
+
 /* Does namelen bytes of name exactly match the symbol? */
 static bool sym_is(const char *name, unsigned namelen, const char *symbol)
 {
@@ -1585,6 +1599,7 @@ static const struct devtable devtable[] = {
 	{"cdx", SIZE_cdx_device_id, do_cdx_entry},
 	{"vchiq", SIZE_vchiq_device_id, do_vchiq_entry},
 	{"coreboot", SIZE_coreboot_device_id, do_coreboot_entry},
+	{"qrtr", SIZE_qrtr_device_id, do_qrtr_entry},
 };
 
 /* Create MODULE_ALIAS() statements.
