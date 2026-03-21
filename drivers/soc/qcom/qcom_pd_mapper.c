d34f4587d7afd32e01338e27ed0f35fa2b7d0e46
Marc Lainez <marc.lainez@gmail.com>
2026-03-21 22:40:10 +0100

    soc: qcom: pd_mapper: Add MSM8974 ADSP domain mappings and enable SMGR/SNS in defconfig
    Add msm8974_domains[] with adsp_audio_pd, adsp_root_pd and
    adsp_sensor_pd entries for Fairphone 2 (MSM8974) ADSP sensor support.
    
    Enable QCOM_SNS_REG and all IIO_QCOM_SMGR sensor drivers as modules
    in msm8974_defconfig.
    
    Signed-off-by: Marc Lainez <marc.lainez@gmail.com>
    (cherry picked from commit a226f1ecb4bc4066122ab06dd010d680362c4571)
    (cherry picked from commit 57fb2e305ebe60ed0c236e42e5dd1bca174b684c)

--- drivers/soc/qcom/qcom_pd_mapper.c
+++ drivers/soc/qcom/qcom_pd_mapper.c
@@ -367,6 +367,19 @@ static const struct qcom_pdm_domain_data *msm8996_domains[] = {
 	NULL,
 };
 
+static const struct qcom_pdm_domain_data *msm8953_domains[] = {
+	&adsp_audio_pd, &adsp_root_pd, &adsp_sensor_pd,
+	&mpss_root_pd,	&mpss_wlan_pd, NULL,
+};
+
+static const struct qcom_pdm_domain_data *msm8974_domains[] = {
+	&adsp_audio_pd,
+	&adsp_root_pd,
+	&adsp_sensor_pd,
+	NULL,
+};
+
+
 static const struct qcom_pdm_domain_data *msm8998_domains[] = {
 	&mpss_root_pd,
 	&mpss_wlan_pd,
@@ -536,7 +549,8 @@ static const struct of_device_id qcom_pdm_domains[] __maybe_unused = {
 	{ .compatible = "qcom,msm8909", .data = NULL, },
 	{ .compatible = "qcom,msm8916", .data = NULL, },
 	{ .compatible = "qcom,msm8939", .data = NULL, },
-	{ .compatible = "qcom,msm8974", .data = NULL, },
+	{ .compatible = "qcom,msm8953", .data = msm8953_domains, },
+	{ .compatible = "qcom,msm8974", .data = msm8974_domains, },
 	{ .compatible = "qcom,msm8996", .data = msm8996_domains, },
 	{ .compatible = "qcom,msm8998", .data = msm8998_domains, },
 	{ .compatible = "qcom,qcm2290", .data = qcm2290_domains, },
